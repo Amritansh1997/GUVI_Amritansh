249745560616a507d651ffe5e16df4c10bbf322c
Amritansh1997 <35635168+Amritansh1997@users.noreply.github.com>
2018-01-27 19:11:15 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{int n,i,flag=0;
+printf("Enter no.");
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{if(n%i==0)
+{flag=1;
+break;}}
+if(flag==0)
+{printf("%d is prime no.",n);}
+else{printf("%d is not prime no.",n);}
+}
